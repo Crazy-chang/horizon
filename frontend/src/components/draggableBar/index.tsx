162c02b53b729a7e540b612d15afdbe243ccae7e
2bit <54610053+ultrazg@users.noreply.github.com>
2024-07-15 16:37:18 +0800

    save

--- frontend/src/components/draggableBar/index.tsx
+++ frontend/src/components/draggableBar/index.tsx
@@ -1,21 +1,4 @@
-import { useEffect, useState } from 'react'
-import { Environment } from '../../../wailsjs/runtime'
-import { envType } from '@/types/env'
-import { TrafficLightButtons } from '@/components'
-
 export const DraggableBar = (): JSX.Element => {
-  const [platform, setPlatform] = useState<string>('')
-
-  useEffect(() => {
-    Environment()
-      .then((res: envType) => {
-        setPlatform(res.platform)
-      })
-      .catch((err: any) => {
-        console.error('err', err)
-      })
-  }, [])
-
   return (
     <div
       style={
@@ -27,8 +10,6 @@ export const DraggableBar = (): JSX.Element => {
           userSelect: 'none',
         } as any
       }
-    >
-      {platform === 'windows' && <TrafficLightButtons />}
-    </div>
+    />
   )
 }
