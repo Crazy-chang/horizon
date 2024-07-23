12754dd87135232f36a0e98250a55a56ae918e5b
2bit <54610053+ultrazg@users.noreply.github.com>
2024-07-23 18:01:56 +0800

    feat: change colorful shadow component's styles.

--- frontend/src/components/colorfulShadow/index.tsx
+++ frontend/src/components/colorfulShadow/index.tsx
@@ -1,28 +1,52 @@
 import React from 'react'
 import './index.modules.scss'
+import { PlayIcon } from '@radix-ui/react-icons'
 
 interface ColorfulShadowType {
   style?: React.CSSProperties
   src: string
+  mask?: boolean
+  curPointer?: boolean
 }
 
-export const ColorfulShadow: React.FC<ColorfulShadowType> = (
-  props
-): JSX.Element => {
+/**
+ * 封面展示组件
+ * @param style React.CSSProperties
+ * @param src 封面 url
+ * @param mask 是否展示遮罩
+ * @param curPointer 光标指针是否展示为“手型”
+ * @returns
+ */
+export const ColorfulShadow: React.FC<ColorfulShadowType> = ({
+  style,
+  src,
+  mask = false,
+  curPointer = false,
+}): JSX.Element => {
   return (
     <div
       className="colorful-shadow-layout"
-      style={props.style}
+      style={style}
     >
-      <div className="pic-box">
+      <div
+        className="pic-box"
+        style={{ cursor: `${curPointer ? 'pointer' : 'default'}` }}
+      >
         <img
-          src={props.src}
+          src={src}
           className="origin_pic"
         />
         <img
-          src={props.src}
+          src={src}
           className="shadow"
         />
+        {mask && (
+          <div className="mask">
+            <div className="circle">
+              <PlayIcon />
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- frontend/src/pages/home/components/popular/index.tsx
+++ frontend/src/pages/home/components/popular/index.tsx
@@ -15,7 +15,7 @@ const mockData = [
   {
     id: 2,
     episode_img:
-      'https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop',
+      'https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png',
     title: 'Vol.55 如何拯救自己的倾诉欲',
     podcast: '不把天聊si',
     description: '评论数 99+',
@@ -47,7 +47,11 @@ const PopularPart = () => {
           >
             <div className="popular-info">
               <div className="cover-box">
-                <ColorfulShadow src={item.episode_img} />
+                <ColorfulShadow
+                  src={item.episode_img}
+                  mask
+                  curPointer
+                />
               </div>
               <div className="info-box">
                 <p>{item.podcast}</p>
