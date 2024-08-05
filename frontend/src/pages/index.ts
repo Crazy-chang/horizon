41a5739804aeeda36f409576165e384ff118b680
2bit <742005790@qq.com>
2024-08-06 00:13:28 +0800

    feat: 搜索页面

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,6 @@
 import { Home } from './home'
 import { Login } from './login'
 import { About } from './about'
+import { Search } from './search'
 
-export { Home, Login, About }
+export { Home, Login, About, Search }
---
+++ frontend/src/pages/search/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './index.modules.scss'
+
+export const Search = () => {
+  return (
+    <div className="search-layout">
+      <h3>搜索</h3>
+    </div>
+  )
+}
