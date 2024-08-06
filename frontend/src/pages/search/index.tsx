aadc2be4134ac4e746a566359bbca5710d373695
2bit <54610053+ultrazg@users.noreply.github.com>
2024-08-06 17:20:40 +0800

    feat: 搜索页面搜索框

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -1,10 +1,33 @@
-import React from 'react'
 import './index.modules.scss'
+import { Button, TextField } from '@radix-ui/themes'
+import { MagnifyingGlassIcon } from '@radix-ui/react-icons'
 
 export const Search = () => {
   return (
     <div className="search-layout">
       <h3>搜索</h3>
+
+      <div className="search-content">
+        <div className="search-input">
+          <div className="left">
+            <TextField.Root
+              size="3"
+              placeholder="Search the docs…"
+            >
+              <TextField.Slot>
+                <MagnifyingGlassIcon
+                  height="16"
+                  width="16"
+                />
+              </TextField.Slot>
+            </TextField.Root>
+          </div>
+
+          <div className="right">
+            <Button size="3">搜索</Button>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
