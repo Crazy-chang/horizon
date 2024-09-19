0706913172d744277a31b2aa02608a46b442d2d9
2bit <742005790@qq.com>
2024-09-20 00:16:35 +0800

    feat: 搜索页「单集」

--- frontend/src/pages/search/index.tsx
+++ frontend/src/pages/search/index.tsx
@@ -4,11 +4,11 @@ import {
   Button,
   Tabs,
   TextField,
-  Text,
   ScrollArea,
   Avatar,
 } from '@radix-ui/themes'
 import { MagnifyingGlassIcon, PlusIcon } from '@radix-ui/react-icons'
+import { SlEarphones, SlBubble } from 'react-icons/sl'
 import { ColorfulShadow } from '@/components'
 
 export const Search = () => {
@@ -172,7 +172,158 @@ export const Search = () => {
                 </Tabs.Content>
 
                 <Tabs.Content value="2">
-                  <Text size="2">Access and update your documents.</Text>
+                  <div className="search-result-episode-layout">
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                    <div className="search-result-episode-item">
+                      <div className="left">
+                        <ColorfulShadow
+                          className="episode-cover"
+                          curPointer
+                          mask
+                          src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+                        />
+                      </div>
+                      <div className="right">
+                        <p>小米SU7营销复盘：你所知道的为什么都是错的-Vol 46</p>
+                        <p>
+                          本期节目关注风口上的小米汽车，主播借助在营销、产品上的经验解答。欢迎在评论区留言发表你对小米汽车的感受与看法，对于节目话题的更多观点，获取更多未呈现在节目中的扩展阅读，欢迎加群讨论
+                        </p>
+                        <p>
+                          <span>30分钟 · 03/29</span>
+                          <span>
+                            <SlEarphones />
+                            4.3万+
+                            <SlBubble />
+                            349
+                          </span>
+                        </p>
+                      </div>
+                    </div>
+                  </div>
                 </Tabs.Content>
 
                 <Tabs.Content value="3">
