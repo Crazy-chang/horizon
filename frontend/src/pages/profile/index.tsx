55b643dadf91170ceddfbf9caddb244a6507c4ee
2bit <742005790@qq.com>
2024-10-04 22:51:17 +0800

    feat: profile 页面功能添加

--- frontend/src/pages/profile/index.tsx
+++ frontend/src/pages/profile/index.tsx
@@ -1,12 +1,58 @@
 import { useRef } from 'react'
+import { Avatar, Flex, Separator } from '@radix-ui/themes'
 import './index.modules.scss'
 
 export const Profile = () => {
-  const elementRef = useRef(null)
+  // const elementRef = useRef(null)
 
   return (
     <div className="profile-layout">
-      <div ref={elementRef}>Profile</div>
+      <div className="profile-content">
+        <div className="profile-avatar">
+          <Avatar
+            size="9"
+            src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
+            fallback="A"
+          />
+        </div>
+        <div className="profile-info">
+          <div className="profile-nickname">hexdream</div>
+          <Separator
+            my="3"
+            size="4"
+          />
+          <div className="profile-follow">
+            <Flex
+              gap="3"
+              align="center"
+            >
+              <div className="chunk">
+                <p>1</p>
+                <p>关注</p>
+              </div>
+              <Separator
+                className="spearator"
+                size="2"
+                orientation="vertical"
+              />
+              <div className="chunk">
+                <p>0</p>
+                <p>粉丝</p>
+              </div>
+              <Separator
+                className="spearator"
+                size="2"
+                orientation="vertical"
+              />
+              <div className="chunk">
+                <p>9</p>
+                <p>订阅</p>
+              </div>
+            </Flex>
+          </div>
+          <div className="profile-bio">还没有设置签名</div>
+        </div>
+      </div>
     </div>
   )
 }
