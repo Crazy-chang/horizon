0ba8b3fdd0623792d3f5f8ca1b45400ff62f2e8f
2bit <742005790@qq.com>
2024-10-10 00:30:24 +0800

    save mileage modal in profile

--- frontend/src/pages/profile/components/mileageModal/index.tsx
+++ frontend/src/pages/profile/components/mileageModal/index.tsx
@@ -2,20 +2,33 @@ import React, { useState } from 'react'
 import { Modal } from '@/components'
 import { modalType } from '@/types/modal'
 import './index.modules.scss'
-import { Button, Dialog, Flex } from '@radix-ui/themes'
+import { Button, Dialog, Flex, Separator } from '@radix-ui/themes'
 
 /**
  * 收听数据弹窗
  */
 export const MileageModal: React.FC<modalType> = ({ open, onClose }) => {
-  const [content, setContent] = useState('')
   return (
     <Modal
       title="收听数据"
       open={open}
       onClose={onClose}
     >
-      {content || <div>暂无数据</div>}
+      <div className="tagline-layout">
+        <div>
+          <span className="num">144</span>小时<span className="num">52</span>
+          分钟
+        </div>
+        <div>木星上已经过去了14天</div>
+      </div>
+
+      <Separator
+        my="3"
+        size="4"
+      />
+
+      <div>lists</div>
+
       <Flex
         gap="3"
         mt="4"
