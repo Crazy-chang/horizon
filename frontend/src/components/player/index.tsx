f24df0df6815c26d7b90fc6513771a07afe8dca4
hexdream <742005790@qq.com>
2024-10-12 16:36:31 +0800

    save player drawer

--- frontend/src/components/player/index.tsx
+++ frontend/src/components/player/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
-import { Drawer } from 'antd'
-import type { DrawerStyles } from 'antd/es/drawer/DrawerPanel'
+import { Button } from '@radix-ui/themes'
 import { useDisplayInfo } from '@/hooks'
 import './index.modules.scss'
 
@@ -9,32 +8,21 @@ interface IProps {
   onClose: () => void
 }
 
-const drawerStyles: DrawerStyles = {
-  header: {
-    '--wails-draggable': 'drag',
-    cursor: 'default',
-    userSelect: 'none',
-  } as any,
-  // content: {
-  //   opacity: 0.5, // FIXME
-  // },
-}
-
 export const Player: React.FC<IProps> = ({ open, onClose }) => {
-  const [height, _] = React.useState<number>(useDisplayInfo().Height - 35)
+  const [height] = React.useState<number>(useDisplayInfo().Height - 35)
+  const [width] = React.useState<number>(useDisplayInfo().Width)
 
   return (
-    <Drawer
-      className="my-drawer"
-      onClose={onClose}
-      open={open}
-      placement="bottom"
-      height={height}
-      styles={drawerStyles}
-      maskClosable={false}
-      mask={false}
+    <div
+      style={
+        open
+          ? { width, height, transform: `translateY(-${height}px)` }
+          : { width, height, transform: `translateY(${height}px)` }
+      }
+      className="player-drawer-layout"
     >
-      <p>player</p>
-    </Drawer>
+      drawer-{height}
+      <Button onClick={onClose}>close</Button>
+    </div>
   )
 }
