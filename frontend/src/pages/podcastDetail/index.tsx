af36990163861456b2084057f365f075754a2ad9
hexream <742005790@qq.com>
2024-10-21 00:24:33 +0800

    save

--- frontend/src/pages/podcastDetail/index.tsx
+++ frontend/src/pages/podcastDetail/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { NavBackButton } from '@/components'
-import './index.modules.scss'
 import { useLocation } from 'react-router-dom'
-import { Box, Flex } from '@radix-ui/themes'
+import { AspectRatio, Box, Flex, Heading, Text } from '@radix-ui/themes'
+import './index.modules.scss'
 
 export const PodcastDetail = () => {
   const { id } = useLocation().state
@@ -11,21 +11,47 @@ export const PodcastDetail = () => {
     <div className="podcast-detail-layout">
       <NavBackButton />
 
-      <div>
-        <Flex gap="3">
+      <div className="podcast-detail-info">
+        <div className="podcast-cover">
           <Box
-            width="64px"
-            height="64px"
+            width="20rem"
+            height="20rem"
           >
-            left
+            <AspectRatio ratio={1 / 1}>
+              <img
+                src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
+                alt="A house in a forest"
+                style={{
+                  objectFit: 'cover',
+                  width: '20rem',
+                  height: '20rem',
+                  borderRadius: 'var(--radius-4)',
+                }}
+              />
+            </AspectRatio>
           </Box>
+        </div>
+        <div className="podcast-detail">
           <Box
-            width="64px"
-            height="64px"
+            mt="6"
+            width="100%"
+            height="100%"
           >
-            right
+            <Heading
+              size="8"
+              align="center"
+            >
+              忽左忽右
+            </Heading>
+            <Text
+              align="center"
+              as="div"
+              size="6"
+            >
+              “忽左忽右”是一档文化沙龙类播客节目，试图为中文播客听众提供基于经验视角的话题和内容。本节目由JustPod出品
+            </Text>
           </Box>
-        </Flex>
+        </div>
       </div>
     </div>
   )
