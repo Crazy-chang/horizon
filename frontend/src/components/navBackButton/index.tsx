2a3aaee3ffdc8c888d7a5d2d064758b43b1d5291
hexream <742005790@qq.com>
2024-10-21 23:33:51 +0800

    save

--- frontend/src/components/navBackButton/index.tsx
+++ frontend/src/components/navBackButton/index.tsx
@@ -1,12 +1,13 @@
 import { useBack } from '@/hooks'
 import { Button } from '@radix-ui/themes'
 import { ChevronLeftIcon } from '@radix-ui/react-icons'
+import './index.modules.scss'
 
 export const NavBackButton = () => {
   const back = useBack()
 
   return (
-    <div>
+    <div className="nav-back-button">
       <Button
         onClick={back}
         size="1"
