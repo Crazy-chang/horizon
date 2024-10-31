750a581d4feba48f093148c730f3d90680c8cfcf
hexdream <742005790@qq.com>
2024-11-01 00:58:46 +0800

    save

--- frontend/src/components/profileModal/index.tsx
+++ frontend/src/components/profileModal/index.tsx
@@ -1,31 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { Dialog, Avatar, IconButton } from '@radix-ui/themes'
+import React, { useState } from 'react'
+import { Dialog, Avatar, IconButton, Text, ScrollArea } from '@radix-ui/themes'
 import * as VisuallyHidden from '@radix-ui/react-visually-hidden'
-import { Cross1Icon } from '@radix-ui/react-icons'
+import { Cross1Icon, DotsHorizontalIcon } from '@radix-ui/react-icons'
 import { modalType } from '@/types/modal'
 import { useDisplayInfo } from '@/hooks'
 import './index.modules.scss'
-import { envType } from '@/types/env'
-import { Environment } from '../../../wailsjs/runtime'
 
 type IProps = {
   uid: string
 } & modalType
 
 export const ProfileModal: React.FC<IProps> = ({ uid, open, onClose }) => {
-  const [envInfo, setEnvInfo] = useState<envType>()
   const [width] = useState<number>(useDisplayInfo().Width * 0.5)
   const [height] = useState<number>(useDisplayInfo().Height * 0.7)
 
   const avoidDefaultDomBehavior = (e: Event) => {
     e.preventDefault()
   }
-
-  useEffect(() => {
-    Environment().then((res: envType) => {
-      setEnvInfo(res)
-    })
-  }, [])
 
   return (
     <Dialog.Root
@@ -42,36 +33,69 @@ export const ProfileModal: React.FC<IProps> = ({ uid, open, onClose }) => {
           <Dialog.Description />
         </VisuallyHidden.Root>
 
-        <div className="profile-modal-layout">
-          <IconButton
-            style={
-              envInfo?.platform === 'darwin'
-                ? { left: '1rem' }
-                : { right: '1rem' }
-            }
-            className="close-button"
-            onClick={onClose}
-            variant="soft"
-            radius="full"
-          >
-            <Cross1Icon />
-          </IconButton>
+        <ScrollArea
+          type="hover"
+          scrollbars="vertical"
+          style={{ height }}
+        >
+          <div className="profile-modal-layout">
+            <div className="pm-action-button">
+              <IconButton
+                onClick={onClose}
+                variant="soft"
+                radius="full"
+                color="gray"
+              >
+                <Cross1Icon />
+              </IconButton>
 
-          <div className="profile-avatar-layout">
-            <div
-              className="background-image"
-              style={{
-                background: `url(https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop) no-repeat center center / cover`,
-                filter: 'blur(12px)',
-              }}
-            />
-            <Avatar
-              className="profile-avatar"
-              src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-              fallback="A"
-            />
+              <IconButton
+                className="more-button"
+                onClick={onClose}
+                variant="soft"
+                radius="full"
+                color="gray"
+              >
+                <DotsHorizontalIcon />
+              </IconButton>
+            </div>
+
+            <div className="profile-avatar-layout">
+              <div className="pm-ip-loc">IP属地：河北</div>
+              <div
+                className="background-image"
+                style={{
+                  background: `url(https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png) no-repeat center center / cover`,
+                  filter: 'blur(12px)',
+                }}
+              />
+              <Avatar
+                className="profile-avatar"
+                src="https://zos.alipayobjects.com/rmsportal/jkjgkEfvpUPVyRjUImniVslZfWPnJuuZ.png"
+                fallback="A"
+              />
+            </div>
+
+            <div className="profile-detail-layout">
+              <div className="pm-nickname">
+                <Text
+                  as="div"
+                  align="center"
+                  size="6"
+                >
+                  晚上吃桃子
+                </Text>
+                <Text
+                  as="div"
+                  align="center"
+                  size="2"
+                >
+                  还没有设置签名
+                </Text>
+              </div>
+            </div>
           </div>
-        </div>
+        </ScrollArea>
       </Dialog.Content>
     </Dialog.Root>
   )
