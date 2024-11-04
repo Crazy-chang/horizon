f716b148fb141dcd0848c94411a1142ebb81e5da
hexdream <742005790@qq.com>
2024-11-04 17:30:52 +0800

    change

--- frontend/src/pages/podcastDetail/index.tsx
+++ frontend/src/pages/podcastDetail/index.tsx
@@ -1,6 +1,6 @@
 import { ColorfulShadow, NavBackButton } from '@/components'
 import { useLocation } from 'react-router-dom'
-import { AspectRatio, Box, Button, Grid, Heading, Text } from '@radix-ui/themes'
+import { AspectRatio, Box, Button, Heading, Text } from '@radix-ui/themes'
 import { CheckIcon } from '@radix-ui/react-icons'
 import { SlBubble, SlEarphones } from 'react-icons/sl'
 import './index.modules.scss'
@@ -13,68 +13,68 @@ export const PodcastDetail = () => {
       <NavBackButton />
 
       <div className="podcast-detail-info">
-        <div className="podcast-cover">
-          <Box
-            width="20rem"
-            height="20rem"
-          >
-            <AspectRatio ratio={1 / 1}>
-              <img
-                src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
-                alt="A house in a forest"
-                style={{
-                  objectFit: 'cover',
-                  width: '20rem',
-                  height: '20rem',
-                  borderRadius: 'var(--radius-4)',
-                }}
-              />
-            </AspectRatio>
+        <div className="pdi-top">
+          <Box className="pdi-top-cover">
+            <img
+              src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
+              alt="A house in a forest"
+              style={{
+                objectFit: 'cover',
+                width: '20rem',
+                height: '20rem',
+                borderRadius: 'var(--radius-6)',
+              }}
+            />
           </Box>
+
+          <div className="pdi-top-description">
+            <div>
+              <Heading
+                size="9"
+                align="left"
+              >
+                忽左忽右
+              </Heading>
+              <Text
+                align="left"
+                as="div"
+                mt="4"
+                mb="4"
+                size="5"
+                style={{ fontWeight: '300' }}
+              >
+                “忽左忽右”是一档文化沙龙类播客节目，试图为中文播客听众提供基于经验视角的话题和内容。本节目由JustPod出品
+              </Text>
+              <div className="sub">
+                <Text
+                  size="5"
+                  mr="5"
+                  style={{ fontWeight: '300' }}
+                >
+                  758554订阅
+                </Text>
+                <Button variant="soft">
+                  <CheckIcon />
+                  已订阅
+                </Button>
+              </div>
+            </div>
+          </div>
         </div>
+
         <div className="podcast-detail">
           <Box
             mt="6"
             width="100%"
             height="100%"
           >
-            <Heading
-              size="9"
-              align="center"
-            >
-              忽左忽右
-            </Heading>
-            <Text
-              align="center"
-              as="div"
-              mt="4"
-              mb="4"
-              size="5"
-              style={{ fontWeight: '300' }}
-            >
-              “忽左忽右”是一档文化沙龙类播客节目，试图为中文播客听众提供基于经验视角的话题和内容。本节目由JustPod出品
-            </Text>
-            <div className="sub">
-              <Text
-                size="5"
-                mr="5"
-                style={{ fontWeight: '300' }}
-              >
-                758554订阅
-              </Text>
-              <Button variant="soft">
-                <CheckIcon />
-                已订阅
-              </Button>
-            </div>
-
             <div className="podcasters">
               <div className="label">节目主播</div>
 
               <div className="layout">
                 <div className="podcaster">
                   <div className="top">
-                    <AspectRatio ratio={1 / 1}>
+                    <AspectRatio ratio={1}>
                       <img
                         src="https://images.unsplash.com/photo-1479030160180-b1860951d696?&auto=format&fit=crop&w=1200&q=80"
                         alt="A house in a forest"
