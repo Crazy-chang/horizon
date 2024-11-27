168511831dc9332809fd1f677ddec9f0929afec1
hexdr3am <742005790@qq.com>
2024-11-28 00:06:06 +0800

    Feat: 我的订阅

--- frontend/src/pages/subscription/index.tsx
+++ frontend/src/pages/subscription/index.tsx
@@ -1,43 +1,91 @@
+import React, { useEffect, useState } from 'react'
+import { AspectRatio, Box, Card, Grid, Text, Spinner } from '@radix-ui/themes'
+import { subscription } from '@/api/subscription'
+import dayjs from 'dayjs'
 import './index.modules.scss'
-import { AspectRatio, Box, Card, Grid, Text } from '@radix-ui/themes'
 
-export const Subscription = () => {
+// TODO: 分页
+export const Subscription: React.FC = () => {
+  const [lists, setLists] = useState<any>([])
+  const [loading, setLoading] = useState<boolean>(false)
+
+  /**
+   * 获取订阅列表数据
+   */
+  const getData = () => {
+    setLoading(true)
+    subscription({})
+      .then((res) => setLists(res.data.data))
+      .catch((err) => {
+        console.error(err)
+      })
+      .finally(() => setLoading(false))
+  }
+
+  useEffect(() => {
+    getData()
+  }, [])
+
   return (
     <div className="subscription-layout">
-      <h3>我的订阅</h3>
+      <h3>我的订阅{lists.length > 0 ? `(${lists.length})` : null}</h3>
 
-      <Grid
-        columns="4"
-        gap="6"
-        width="auto"
-      >
-        <Box>
-          <Card className="podcast-cover">
-            <AspectRatio ratio={8 / 8}>
-              <img
-                src="https://images.unsplash.com/photo-1502823403499-6ccfcf4fb453?&w=256&h=256&q=70&crop=focalpoint&fp-x=0.5&fp-y=0.3&fp-z=1&fit=crop"
-                className="podcast-img"
-              />
-            </AspectRatio>
+      {loading ? (
+        <Spinner size="3" />
+      ) : lists.length === 0 ? (
+        <div
+          style={{
+            width: '100%',
+            height: '80%',
+            color: 'gray',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          暂无数据
+        </div>
+      ) : (
+        <Grid
+          columns="4"
+          gap="6"
+          width="auto"
+          mb="6"
+        >
+          {lists.map((item: any) => (
+            <Box key={item.pid}>
+              <Card
+                className="podcast-cover"
+                title={item.title}
+              >
+                <AspectRatio ratio={8 / 8}>
+                  <img
+                    src={item.image.picUrl}
+                    className="podcast-img"
+                    alt={item.title}
+                  />
+                </AspectRatio>
 
-            <Text
-              mt="3"
-              className="podcast-title"
-              as="p"
-              size="4"
-            >
-              Web Worker - 前端程序员都爱听
-            </Text>
-            <Text
-              className="podcast-update-time"
-              as="p"
-              size="2"
-            >
-              更新于09/05
-            </Text>
-          </Card>
-        </Box>
-      </Grid>
+                <Text
+                  mt="3"
+                  className="podcast-title"
+                  as="p"
+                  size="4"
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  className="podcast-update-time"
+                  as="p"
+                  size="2"
+                >
+                  更新于{dayjs(item.latestEpisodePubDate).format('MM/DD')}
+                </Text>
+              </Card>
+            </Box>
+          ))}
+        </Grid>
+      )}
     </div>
   )
 }
